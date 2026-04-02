c477bb8415838dd35d3f01aa5fb079954ca3bf34
Eryas <eryasnouri@gmail.com>
2026-04-02 11:19:15 +0200

    Update unit_tests.py

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -1,9 +1,20 @@
 import unittest
-from cashpro.reduction import *
+from cashpro.reduction import calculerReduction
 
 class calculerReductionUnitTest(unittest.TestCase):
-  def test_exemple(self):
-    self.assertEqual(True, False)
-
-if __name__ == '__main__':
-  unittest.main()
+    def test_calculerReduction(self):
+        #cas 1: montant 15, fidélité Vrai -> 0%
+        self.assertEqual(calculerReduction(15.0, True), 0)     
+        #Cas 2: Montant 32, fidélité Vrai -> 5%
+        self.assertEqual(calculerReduction(32.0, True), 5)  
+        #Cas 3: Montant 100, fidélité Vrai -> 10%
+        self.assertEqual(calculerReduction(100.0, True), 10)   
+        #cas 4: Montant 201, fidélité Vrai -> 15%
+        self.assertEqual(calculerReduction(201.0, True), 15)    
+        #Cas 5: Montant 200, fidélité Faux -> 0%
+        self.assertEqual(calculerReduction(200.0, False), 0) 
+        #cas 6: Montant 50, fidélité Faux -> 0%
+        self.assertEqual(calculerReduction(50.0, False), 0)  
+        #Cas 7: Montant -10, fidélité vrai -> error (ValueError)
+        with self.assertRaises(ValueError): 
+            calculerReduction(-10.0, True) 

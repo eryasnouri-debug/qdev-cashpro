83b225aa7f5241391c41389010a888925898f295
Eryas <eryasnouri@gmail.com>
2026-04-02 11:11:43 +0200

    Update reduction.py

--- cashpro/reduction.py
+++ cashpro/reduction.py
@@ -1,2 +1,14 @@
-def calculerReduction(total, fidelite):
-  pass
+def calculerReduction(montant_total, est_fidele):
+    if montant_total < 0:
+        raise ValueError("Le montant total ne peut pas être inférieur à 0,00 €") 
+    if not est_fidele:
+        return 0
+    # Barème pour les clients adhérents 
+    if montant_total >= 200.0:
+        return 15 
+    elif montant_total >= 100.0:
+        return 10
+    elif montant_total >= 30.0:
+        return 5 
+    else:
+        return 0 
